5a9213327beba8ea3484d761b85d3fe35c90418b
Aravind-22 <aravindanbinga@gmail.com>
2016-06-23 19:21:03 +0530

    Create posneg.java

---
+++ posneg.java
@@ -0,0 +1,21 @@
+class posneg 
+{ 
+public static void main(String args[]) 
+{ 
+int n; 
+Scanner s=new Scanner(System.in); 
+System.out.println("enter the number");
+n=s.nextInt(); 
+if(n>0) 
+{ 
+System.out.println("number is positive");
+} 
+elseif(n<0) 
+{ 
+System.out.println("number is negative");
+} 
+else 
+{ 
+System.out.println("zero"); 
+} 
+}}
