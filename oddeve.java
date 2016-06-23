52684140f3c212c5aed5532b6b2dc489431a3c7a
Aravind-22 <aravindanbinga@gmail.com>
2016-06-23 19:48:00 +0530

    Create oddeve.java

---
+++ oddeve.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class oddeve
+{
+public Static void main(String args[])
+{
+int n;
+Scanner s=new Scanner(System.in);
+System.out.println("enter a number");
+n=s.nextInt();
+if(n%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}}
