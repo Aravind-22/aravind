5dd4cc0fdd69f1f48eebc7d0dbebc8e772730755
Aravind-22 <aravindanbinga@gmail.com>
2016-06-23 21:47:02 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,30 @@
+class vowel
+{
+public static void main(String args[])
+{
+char ch='u';
+if(ch='a')
+{
+System.out.println("vowel");
+}
+elseif(ch='e')
+{
+System.out.println("vowel");
+}
+elseif(ch='i')
+{
+System.out.println("vowel");
+}
+elseif(ch='o')
+{
+System.out.println("vowel");
+}
+elseif(ch='u')
+{
+System.out.println("vowel");
+}
+else
+{
+System.out.println("Consonant")'
+}
+}}
