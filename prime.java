5074a764d0f66c96919643a7fdc69bf7958662b2
Aravind-22 <aravindanbinga@gmail.com>
2016-06-24 22:07:51 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,21 @@
+class prime
+{
+public void Prime(int n)
+{
+boolean isPrime = true;
+for (int i = 2; i < n; i++) 
+{
+ if (n % i == 0) 
+ {
+ isPrime = false;
+ break;
+ }
+ }
+ if (isPrime) 
+ {
+ System.out.println(n + " is a prime number");
+ } 
+ else 
+ {
+ System.out.println(n + " is not a prime number");
+ }}}
